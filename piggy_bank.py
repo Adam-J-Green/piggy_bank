2142d813faa2c65777d412f609a3efe8f5256837
Adam Green <adamgreener10@gmail.com>
2023-08-25 09:11:08 -0400

    Final Changes to App

--- piggy_bank.py
+++ piggy_bank.py
@@ -3,7 +3,10 @@ import pandas as pd
 import piggy_bank_funcs as pb
 from PIL import Image
 
+
 def main():
+    st.title('The Dank Piggy Bank')
+    st.write('Thank you for your contrinbutions to the family piggy bank. These dollars are going to make the world a better place. \nAs a thank you, I welcome you to submit your email to be entered into a draw for a fun giveaway. \nDraws will happen on the last Friday of every month with the value of the prize reflecting the piggy bank contributions for the month.')
     im = Image.open('piggy.png')
     col1, col2 = st.columns([1,1])
 
@@ -12,8 +15,8 @@ def main():
 
     with col2:
         pb.input_email()
-
-    pb.send_email()
+        if st.button('Submit Entry'):
+            pb.send_email()
 
 
 if __name__ == '__main__':
